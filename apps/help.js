f385816fe5b142c9e9476f278f20c4cad88c9498
wuliya990 <postmaster@wuliya.icu>
2024-03-28 15:54:11 +0800

    🎈 perf: 细节优化

--- apps/help.js
+++ apps/help.js
@@ -30,7 +30,6 @@ export class help extends plugin {
     }
 
     async help (e) {
-      e.msg.replace(/#?(星点签名|starlight-qsign)(命令|帮助|菜单|help|说明|功能|指令|使用说明)/gi, '').trim().toLowerCase();
         let custom = {}
         let help = {}
         let { diyCfg, sysCfg } = await Data.importCfg('help')
@@ -76,7 +75,7 @@ export class help extends plugin {
         }, { e, scale: 1.2 });
         await e.reply([
           helpimg,
-          this.button
+          button
         ])
     }     
       async versionInfo (e) {
